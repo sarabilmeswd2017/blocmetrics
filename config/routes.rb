7af61a400181d8d4df2d42b3fd7da8816ecc1965
Sara Bilmes <saraesrig@gmail.com>
2017-10-01 13:55:04 +0300

    api-controller:attempt 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :registered_applications
+  namespace :api, defaults: { format: :json } do
+     match '/events', to: 'events#preflight', via: [:options]
+     resources :events, only: [:create]
+   end
   authenticated :user do
     root 'registered_applications#index', as: :authenticated_root
   end
